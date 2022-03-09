3eed4583af7b0c4fc091f59ae6abfb65432fd6b4
Cinimajig <39052490+Cinimajig@users.noreply.github.com>
2022-03-09 21:39:56 +0100

    Removed commented code

--- ProfileSetup/Setup.ps1
+++ ProfileSetup/Setup.ps1
@@ -5,15 +5,3 @@ Get-ChildItem $PSScriptRoot -Directory -Exclude "_*" | ForEach-Object {
     Write-Host "Running", $_.BaseName, "..."
     . $SetupScript
 }
-
-# # Setup PowerShell profiles.
-# . "$PSScriptRoot\PowerShell_Profiles\Setup.ps1"
-
-# # Setup console colors with defaults.
-# . "$PSScriptRoot\Console_Colors\Setup.ps1"
-
-# # Setup bin folder in userspace, for easy access to scripts.
-# . "$PSScriptRoot\Bin_Path\Setup.ps1"
-
-# # Install Scripts file for easy admin prompts and .vbs debugging.
-# . "$PSScriptRoot\VBS_Magic\Setup.ps1"
