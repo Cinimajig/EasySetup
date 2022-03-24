f0df8376ee1197ced98090e66b3961fb04aa49cc
Cinimajig <jacob.mogensen5@gmail.com>
2022-03-24 19:29:27 +0100

    Fixed PowerShell_Profiles syntax error

--- ProfileSetup/1_PowerShell_Profiles/Setup.ps1
+++ ProfileSetup/1_PowerShell_Profiles/Setup.ps1
@@ -1,7 +1,11 @@
 $ProfileFolder = "$env:USERPROFILE\Documents\WindowsPowerShell\"
 
+if (-not (Test-Path $ProfileFolder)) {
+    New-Item $ProfileFolder -ItemType Directory -Force | Out-Null
+}
+
 Get-ChildItem "$PSScriptRoot\Profiles" | ForEach-Object {
-    Copy-Item -Path $_.FullName-Destination $ProfileFolder -Force
+    Copy-Item -Path $_.FullName -Destination $ProfileFolder -Force
 }
 
 Copy-Item -Path "$PSScriptRoot\user.config" -Destination "$env:LOCALAPPDATA\Microsoft_Corporation\powershell_ise.exe_StrongName_lw2v2vm3wmtzzpebq33gybmeoxukb04w\3.0.0.0" -Force
